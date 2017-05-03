71dd1c61f5b608f031baa403e3463eadec645bac
wangqinglong <tsingcoo.wang@gmail.com>
2017-05-03 15:03:20 +0800

    version 0.1

--- process.py
+++ process.py
@@ -1,20 +1,32 @@
 # coding:utf-8
+import jieba
+import sys
+
+
+# reload(sys)
+# sys.setdefaultencoding("utf-8")
+
 
 def split_en_ch(filename):
     # i = 0
     en = []
+    ch = []
     with open(filename, 'r') as f:
         line = f.readline()  # 跳过第一行
         for line in f.readlines():
             # i += 1
-            # if i < 100:
-            # print(line.split('\t')[0])
+            # if i < 14632:
+            #     print(line.split('\t')[1])
             en.append(line.split('\t')[0])
-    return en
+            ch.append(line.split('\t')[1])
+    return en, ch
 
 
-def get_key_words(en):
-    many_word_line = 0
+def get_key_words(en):  # 返回关键词和对应的行号，跳过第一行，下标从0开始
+    key_words = []
+    many_word_line = 0  # 包含多个词的行
+    one_word_line = 0  # 只出现一次的词的行，从0开始
+    one_word_index = []
     for line in en:
         # print(line)
         words = line.split(" ")
@@ -22,16 +34,163 @@ def get_key_words(en):
         for word in words:
             if "<vocab>" in word:
                 i += 1
-        if i > 1:
-            print(line)
-            print(i)
+        if i > 1:  # 包含多个词
+            # print(line)
+            # print(i)
             many_word_line += 1
-    print(many_word_line)
+        else:  # 仅仅一个词
+            for word in words:
+                if "<vocab>" in word:
+                    word = word.split("<vocab>")[1]  # 去掉关键词左边的部分
+                    word = word.split("</vocab>")[0]  # 去掉关键词右边的部分
+                    word = word.lower()
+                    key_words.append(word)
+                    one_word_index.append(one_word_line)
+        one_word_line += 1  # 记录行标
+
+    # print(key_words)
+    # print(len(key_words))
+    # print(one_word_index)
+    # print(len(one_word_index))
+    return key_words, one_word_index
+
+
+def word_seg(ch, ch_seg_filename):
+    ch_seg = []
+    with open(ch_seg_filename, "w") as f:
+        for line in ch:
+            seg_list = list(jieba.cut(line))
+            f.writelines(" ".join(seg_list))
+            # ch_seg.append(" ".join(seg_list))
+            # print ch_seg
+            # return ch_seg
+
+
+def construct_keyword_ch(key_words, one_word_index, ch_seg_finename):
+    ch_seg = []
+    ch_seg_of_key_words = []
+    with open(ch_seg_finename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            # print (line)
+            line = line.split(" ")
+            # print (line)
+            ch_seg.append(line)
+    # print (ch_seg)
+    for i in range(len(one_word_index)):
+        ch_seg_of_key_words.append(ch_seg[i])
+    # print((ch_seg_of_key_words))
+    return ch_seg_of_key_words
+
+
+def word2index(key_words, vocab_filename):
+    vocab = {}
+    with open(vocab_filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(" ")
+            vocab[line[1]] = line[0]
+
+    for i in range(len(key_words)):
+        if key_words[i] in vocab:
+            index = vocab[key_words[i]]
+        else:
+            index = '-1'
+        key_words[i] = index
+    # print(key_words)
+    return key_words
+
+
+def ch_seg2index(ch_seg, vocab_filename):
+    vocab = {}
+    with open(vocab_filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(" ")
+            vocab[line[1]] = line[0]
+    for i in range(len(ch_seg)):
+        for j in range(len(ch_seg[i])):
+            if ch_seg[i][j] in vocab:
+                index = vocab[ch_seg[i][j]]
+            else:
+                index = '-1'
+            ch_seg[i][j] = index
+    # print(ch_seg)
+    return ch_seg
+
+
+def get_align(key_words_index, ch_seg_of_key_words_index, prob_filename):
+    align = []
+    prob = {}
+    with open(prob_filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(" ")
+            if line[0] in prob:
+                prob[line[0]][line[1]] = float(line[2])
+            else:
+                prob[line[0]] = {}
+                prob[line[0]][line[1]] = float(line[2])
+    # print(prob)
+    for i in range(len(key_words_index)):
+        tmp = 0
+        tar = '-1'
+        if key_words_index[i] == '-1':
+            # print('-1'+' '+'-1')
+            align.append('-1' + ' ' + '-1')
+        else:
+            for j in range(len(ch_seg_of_key_words_index[i])):
+                if ch_seg_of_key_words_index[i][j] != '-1':
+                    if key_words_index[i] in prob:
+                        if ch_seg_of_key_words_index[i][j] in prob[key_words_index[i]]:
+                            if prob[key_words_index[i]][ch_seg_of_key_words_index[i][j]] > tmp:
+                                tmp = prob[key_words_index[i]][ch_seg_of_key_words_index[i][j]]
+                                tar = ch_seg_of_key_words_index[i][j]
+            # print(key_words_index[i] + ' '+ tar)
+            align.append(key_words_index[i] + ' ' + tar)
+    # print(align)
+    return align
+
+
+def align2word(align, vocab_en_filename, vocab_ch_filename):
+    vocab_en = {}
+    vocab_ch = {}
+    with open(vocab_en_filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(" ")
+            vocab_en[line[0]] = line[1]
+    with open(vocab_ch_filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(" ")
+            vocab_ch[line[0]] = line[1]
+
+    for i in range(len(align)):
+        align_line = align[i].split(" ")
+        if align_line[0] == "-1":
+            word_en = "-1"
+        else:
+            word_en = vocab_en[align_line[0]]
+        if align_line[1] == "-1":
+            word_ch = "-1"
+        else:
+            word_ch = vocab_ch[align_line[1]]
+        print(word_en + " " + word_ch)
 
 
 def main():
-    en = split_en_ch("/Users/wangqinglong/Library/Mobile Documents/com~apple~CloudDocs/Shanbay/examples.txt")
-    get_key_words(en)
+    en, ch = split_en_ch("/Users/wangqinglong/Library/Mobile Documents/com~apple~CloudDocs/Shanbay/examples.txt")
+    key_words, one_word_index = get_key_words(en)
+    word_seg(ch, "/Users/wangqinglong/Library/Mobile Documents/com~apple~CloudDocs/Shanbay/ch_seg.txt")
+    ch_seg_of_key_words = construct_keyword_ch(key_words, one_word_index,
+                                               "/Users/wangqinglong/Library/Mobile Documents/com~apple~CloudDocs/Shanbay/ch_seg.txt")
+    # print(key_words)
+    # print(len(ch_seg_of_key_words))
+    key_words_index = word2index(key_words, "/Users/wangqinglong/Windows/merged.en.vcb")
+    ch_seg_of_key_words_index = ch_seg2index(ch_seg_of_key_words, "/Users/wangqinglong/Windows/merged.ch.vcb")
+    align = get_align(key_words_index, ch_seg_of_key_words_index, "/Users/wangqinglong/Windows/t2s64.thmm.5")
+    align2word(align, "/Users/wangqinglong/Windows/merged.en.vcb", "/Users/wangqinglong/Windows/merged.ch.vcb")
 
 
 if __name__ == '__main__':
